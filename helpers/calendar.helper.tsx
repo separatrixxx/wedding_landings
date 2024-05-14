384f4ff9737fc7ee0f5d9b9cd1eca03e073721b3
separatrixxx <nikitalochmatov@gmail.com>
2024-05-14 17:36:30 +0300

    update calendar

--- helpers/calendar.helper.tsx
+++ helpers/calendar.helper.tsx
@@ -3,6 +3,8 @@ import { setLocale } from "./locale.helper";
 
 
 export function addDateToCalendar(data: DataInterface, router: any) {
+    const formattedDate = data.date.split('T')[0].split('-').reverse().join('');
+
     const dateParts = data.date.split('-');
     const isoFormattedDate = `${dateParts[2]}-${dateParts[1]}-${dateParts[0]}`;
     const dateObject = new Date(isoFormattedDate);
@@ -10,20 +12,15 @@ export function addDateToCalendar(data: DataInterface, router: any) {
     const userAgent = window.navigator.userAgent;
     const isiOS = /iPhone|iPad|iPod/i.test(userAgent);
 
-    const startTime = dateObject.toISOString().replace(/-|:|\.\d{3}/g, '');
-    const endTime = new Date(dateObject.getTime() + 3600000).toISOString().replace(/-|:|\.\d{3}/g, ''); // +1 hour for example
-
     const details = localeData.wedding + ' ' + data.brideName + '&' + data.groomName + ' ' + localeData.at + ' ' + data.time;
     const location = data.location + ', ' + data.restourant;
 
     let url: string;
     if (!isiOS) {
-        // URL for non-iOS devices (Google Calendar)
-        url = `https://www.google.com/calendar/render?action=TEMPLATE&dates=${startTime}/${endTime}&ctz=UTC&details=${encodeURIComponent(details)}&location=${encodeURIComponent(location)}&text=${encodeURIComponent(details)}`;
+        url = `https://www.google.com/calendar/render?action=TEMPLATE&dates=${formattedDate}/${formattedDate}&ctz=UTC&details=${encodeURIComponent(details)}&location=${data.location + ', ' + data.restourant}&text=${localeData.wedding + ' ' + data.brideName + '&' + data.groomName + ' ' + localeData.at + ' ' + data.time}`;
         window.open(url, '_blank');
     } else {
-        // ICS format for iOS devices
-        var icsMSG = `BEGIN:VCALENDAR\nVERSION:2.0\r\nPRODID:-//Your Company//NONSGML v1.0//EN\r\nBEGIN:VEVENT\r\nUID:me@google.com\r\nDTSTAMP:${startTime}\r\nATTENDEE;CN=${data.brideName};RSVP=TRUE:MAILTO:me@gmail.com\r\nORGANIZER;CN=Organizer:MAILTO:me@gmail.com\r\nDTSTART:${startTime}\r\nDTEND:${endTime}\r\nLOCATION:${encodeURIComponent(location)}\r\nSUMMARY:${encodeURIComponent(details)}\r\nEND:VEVENT\r\nEND:VCALENDAR`;
+        var icsMSG = `BEGIN:VCALENDAR\nVERSION:2.0\r\nPRODID:-//Your Company//NONSGML v1.0//EN\r\nBEGIN:VEVENT\r\nUID:me@google.com\r\nDTSTAMP:${formattedDate}\r\nATTENDEE;CN=${data.brideName};RSVP=TRUE:MAILTO:me@gmail.com\r\nORGANIZER;CN=Organizer:MAILTO:me@gmail.com\r\nDTSTART:${formattedDate}\r\nLOCATION:${encodeURIComponent(location)}\r\nSUMMARY:${encodeURIComponent(details)}\r\nEND:VEVENT\r\nEND:VCALENDAR`;
         var title = "newEvent.ics";
         var uri = "data:text/calendar;charset=utf8," + escape(icsMSG);
 
