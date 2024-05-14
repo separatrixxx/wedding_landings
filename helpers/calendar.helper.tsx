4ffa2e57347db43b1478240020154b4165bd5a7b
separatrixxx <nikitalochmatov@gmail.com>
2024-05-14 17:45:14 +0300

    update calendar

--- helpers/calendar.helper.tsx
+++ helpers/calendar.helper.tsx
@@ -5,26 +5,36 @@ import { setLocale } from "./locale.helper";
 export function addDateToCalendar(data: DataInterface, router: any) {
     const formattedDate = data.date.split('T')[0].split('-').reverse().join('');
 
-    const dateParts = data.date.split('-');
-    const isoFormattedDate = `${dateParts[2]}-${dateParts[1]}-${dateParts[0]}`;
-    const dateObject = new Date(isoFormattedDate);
     const localeData = setLocale(router.locale);
     const userAgent = window.navigator.userAgent;
     const isiOS = /iPhone|iPad|iPod/i.test(userAgent);
 
+    const titleText = localeData.wedding + ' ' + data.brideName + '&' + data.groomName;
     const details = localeData.wedding + ' ' + data.brideName + '&' + data.groomName + ' ' + localeData.at + ' ' + data.time;
     const location = data.location + ', ' + data.restourant;
 
     let url: string;
     if (!isiOS) {
-        url = `https://www.google.com/calendar/render?action=TEMPLATE&dates=${formattedDate}/${formattedDate}&ctz=UTC&details=${encodeURIComponent(details)}&location=${data.location + ', ' + data.restourant}&text=${localeData.wedding + ' ' + data.brideName + '&' + data.groomName + ' ' + localeData.at + ' ' + data.time}`;
+        url = `https://www.google.com/calendar/render?action=TEMPLATE&dates=${formattedDate}/${formattedDate}&ctz=UTC&details=${encodeURIComponent(details)}&location=${encodeURIComponent(location)}&text=${encodeURIComponent(titleText)}`;
         window.open(url, '_blank');
     } else {
-        var icsMSG = `BEGIN:VCALENDAR\nVERSION:2.0\r\nPRODID:-//Your Company//NONSGML v1.0//EN\r\nBEGIN:VEVENT\r\nUID:me@google.com\r\nDTSTAMP:${formattedDate}\r\nATTENDEE;CN=${data.brideName};RSVP=TRUE:MAILTO:me@gmail.com\r\nORGANIZER;CN=Organizer:MAILTO:me@gmail.com\r\nDTSTART:${formattedDate}\r\nLOCATION:${encodeURIComponent(location)}\r\nSUMMARY:${encodeURIComponent(details)}\r\nEND:VEVENT\r\nEND:VCALENDAR`;
-        var title = "newEvent.ics";
-        var uri = "data:text/calendar;charset=utf8," + escape(icsMSG);
-
-        var link = document.createElement("a");
+        const icsMSG = `BEGIN:VCALENDAR
+            VERSION:2.0
+            BEGIN:VEVENT
+            UID:me@google.com
+            DTSTAMP:${formattedDate}
+            ATTENDEE;CN=${titleText};
+            DTSTART:${formattedDate}
+            DTEND:${formattedDate}
+            LOCATION:${location}
+            SUMMARY:${details}
+            END:VEVENT
+            END:VCALENDAR`;
+        
+        const title = "newEvent.ics";
+        const uri = "data:text/calendar;charset=utf8," + escape(icsMSG);
+        
+        const link = document.createElement("a");
         link.href = uri;
         link.download = title;
         link.target = "_blank";
