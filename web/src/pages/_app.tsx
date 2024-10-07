f9c5507763ff1f2a58e7be2269356ef3ecff5001
separatrixxx <nikitalochmatov@gmail.com>
2024-10-07 15:17:49 +0300

    update error page color

--- web/src/pages/_app.tsx
+++ web/src/pages/_app.tsx
@@ -1,5 +1,5 @@
 import '../styles/globals.css';
-import '../styles/config.css';
+// import '../styles/config.css';
 import type { AppProps } from 'next/app';
 import Head from 'next/head';
 import { useRouter } from 'next/router';
